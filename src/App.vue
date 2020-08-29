3f2cbbc459f029e9e6a91922ea76010db2d4699e
paatryk01 <paatryk01@yahoo.pl>
2020-08-29 12:27:20 +0200

    first upload

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,16 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+    <Grid />
   </div>
 </template>
 
 <script lang="ts">
 import { Component, Vue } from 'vue-property-decorator';
-import HelloWorld from './components/HelloWorld.vue';
+import Grid from './components/Grid.vue';
 
 @Component({
   components: {
-    HelloWorld,
+    Grid,
   },
 })
 export default class App extends Vue {}
